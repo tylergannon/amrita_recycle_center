e04327a0dbf6e8ae077426d69951bbcb51803c51
Tyler Gannon <tgannon@gmail.com>
2013-12-23 10:41:07 +0530

    Initial Creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AmritaRecycleCenter::Application.config.secret_key_base = '43f073f98fdec2a2537116ed7aebea9f5eb17256f3612c9cb74622a0eac9ad72ae3e9716a91ba8d3aa9e29dce20e73efa40e22b2179eb070992e67d0414e573c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AmritaRecycleCenter::Application.config.session_store :cookie_store, key: '_amrita_recycle_center_session'
