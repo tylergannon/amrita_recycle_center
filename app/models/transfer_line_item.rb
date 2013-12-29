20394e1bb385adec05fe0bf6ee25a4ff57cd3ec6
Tyler Gannon <tgannon@gmail.com>
2013-12-29 12:41:08 +0530

    Transfer Line Items should validate weight.

--- app/models/transfer_line_item.rb
+++ app/models/transfer_line_item.rb
@@ -3,4 +3,11 @@ class TransferLineItem < ActiveRecord::Base
   belongs_to :container
   belongs_to :transfer
   
+  validate :net_weight_should_be_the_difference_between_gross_weight_and_container_weight
+  
+  def net_weight_should_be_the_difference_between_gross_weight_and_container_weight
+    unless net_weight == gross_weight - container.empty_weight
+      errors.add(:net_weight, "Should be difference between gross weight and container weight")
+    end
+  end
 end
--- spec/models/transfer_line_item_spec.rb
+++ spec/models/transfer_line_item_spec.rb
@@ -9,8 +9,18 @@ describe TransferLineItem do
   
   describe "net weight validation" do
     describe "when net weight equals difference between gross_weight and container.empty_weight" do
-      before do
-        
+      let(:container) {create :container, empty_weight: 1}
+      it "should pass validation" do
+        line_item = build :transfer_line_item, container: container, gross_weight: 11, net_weight: 10
+        line_item.should be_valid
+      end
+    end
+
+    describe "when net weight DOES NOT EQUAL difference between gross_weight and container.empty_weight" do
+      let(:container) {create :container, empty_weight: 2}
+      it "should pass validation" do
+        line_item = build :transfer_line_item, container: container, gross_weight: 11, net_weight: 10
+        line_item.should_not be_valid
       end
     end
   end
