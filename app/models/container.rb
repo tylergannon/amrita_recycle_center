2ac8f8f79186365b8a99622c51e6b5af7dba6f67
Tyler Gannon <tgannon@gmail.com>
2014-01-01 07:06:07 +0530

    Don't keep "pending" specs unless they are actually pending.
    And if they're actually pending, write the specs...

--- app/models/container.rb
+++ app/models/container.rb
@@ -1,4 +1,5 @@
 class Container < ActiveRecord::Base
+  validates :name, presence: true
   NO_CONTAINER_NAME = 'No Container (N/A)'
   def self.no_container
     find_by(name: NO_CONTAINER_NAME)
--- spec/models/container_spec.rb
+++ spec/models/container_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 
 describe Container do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject {build :container}
+  it {should validate_presence_of(:name)}
 end
