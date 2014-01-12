1afdd67b74994fc059630af96825fcc13022c0ec
Tyler Gannon <tgannon@gmail.com>
2014-01-12 10:57:39 +0530

    Added default container to material category.

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe Category do
   subject {build :category}
   it {should validate_presence_of(:name)}
-  # it {should belong_to(:default_container)}
-  # it {should validate_presence_of(:default_container)}
+  it {should belong_to(:default_container)}
+  it {should validate_presence_of(:default_container)}
+  
 end
